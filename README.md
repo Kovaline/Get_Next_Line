ff5d16af1cdfde3c4a9dc5bcd60124e11e44f0cd
Ihor KOVALENKO <ikovalen@e2r10p4.unit.ua>
2019-05-02 13:04:51 +0300

    gnl

---
+++ README.md
@@ -0,0 +1,3 @@
+# Get_Next_Line
+
+A function that allows to read a file line by line with different buffer_size
---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikovalen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/01 10:52:59 by ikovalen          #+#    #+#             */
+/*   Updated: 2018/11/08 15:27:59 by ikovalen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+#endif
